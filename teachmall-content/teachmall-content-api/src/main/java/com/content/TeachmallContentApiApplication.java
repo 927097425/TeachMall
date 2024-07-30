7ec9effc74eea142e4662e39b484efd600d5a0c7
xiaochen <927097425@qq.com>
2024-07-30 23:31:41 +0800

    first version

--- teachmall-content/teachmall-content-api/src/main/java/com/content/TeachmallContentApiApplication.java
+++ teachmall-content/teachmall-content-api/src/main/java/com/content/TeachmallContentApiApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class TeachmallContentApiApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class,args);
+        SpringApplication.run(TeachmallContentApiApplication.class, args);
     }
+
 }
