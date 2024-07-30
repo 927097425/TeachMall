7830cfe6ed598967a70afdb824dd47cf0f0f6b5f
xiaochen <927097425@qq.com>
2024-07-30 09:16:21 +0800

    first version

---
+++ teachmall-base/src/main/java/com/teachmall/base/TeachmallBaseApplication.java
@@ -0,0 +1,13 @@
+package com.teachmall.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachmallBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeachmallBaseApplication.class, args);
+    }
+
+}
---
+++ teachmall-base/src/test/java/com/teachmall/base/TeachmallBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachmall.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachmallBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
