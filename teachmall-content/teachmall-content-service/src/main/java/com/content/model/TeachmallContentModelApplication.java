2f8f47a9a1aa4a7b2e1f0505eeb9f982c88b49d0
xiaochen <927097425@qq.com>
2024-07-30 15:39:44 +0800

    first version

---
+++ teachmall-content/teachmall-content-service/src/main/java/com/content/model/TeachmallContentModelApplication.java
@@ -0,0 +1,13 @@
+package com.content.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachmallContentModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeachmallContentModelApplication.class, args);
+    }
+
+}
---
+++ teachmall-content/teachmall-content-service/src/test/java/com/content/model/TeachmallContentModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.content.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachmallContentModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
