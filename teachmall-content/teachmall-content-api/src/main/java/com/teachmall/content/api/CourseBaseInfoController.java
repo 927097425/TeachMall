e596331b5cb2f87d894a4664f3e4103cb947a682
xiaochen <927097425@qq.com>
2024-07-31 16:48:38 +0800

    first version

---
+++ teachmall-content/teachmall-content-api/src/main/java/com/teachmall/content/api/CourseBaseInfoController.java
@@ -0,0 +1,18 @@
+package com.teachmall.content.api;
+
+import com.teachmall.content.api.dto.QueryCourseParamsDto;
+import com.teachmall.content.api.po.CourseBase;
+import com.teachmall.base.model.PageParams;
+import com.teachmall.base.model.PageResult;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseBaseInfoController {
+    @RequestMapping("/course/list")
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
+        return null;
+    }
+
+}
---
+++ teachmall-content/teachmall-content-model/src/main/java/com/teachmall/content/api/TeachmallContentModelApplication.java
@@ -0,0 +1,13 @@
+package com.teachmall.content.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachmallContentModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeachmallContentModelApplication.class, args);
+    }
+
+}
