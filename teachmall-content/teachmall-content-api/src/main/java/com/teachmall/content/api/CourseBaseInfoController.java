6b2cc955e781efe68191764eb4023ec361040399
xiaochen <927097425@qq.com>
2024-07-31 18:26:57 +0800

    first version

--- teachmall-content/teachmall-content-api/src/main/java/com/teachmall/content/api/CourseBaseInfoController.java
+++ teachmall-content/teachmall-content-api/src/main/java/com/teachmall/content/api/CourseBaseInfoController.java
@@ -4,13 +4,19 @@ import com.teachmall.content.api.dto.QueryCourseParamsDto;
 import com.teachmall.content.api.po.CourseBase;
 import com.teachmall.base.model.PageParams;
 import com.teachmall.base.model.PageResult;
+import io.swagger.annotations.Api;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "课程相关接口")
 @RestController
 public class CourseBaseInfoController {
-    @RequestMapping("/course/list")
+    @ApiOperation("查询课程列表")
+    @PostMapping("/course/list")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
         return null;
     }
