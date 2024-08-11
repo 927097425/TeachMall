64a9a44b78ac6cadb244d6e2bf030f1341126c7c
xiaochen <927097425@qq.com>
2024-08-11 22:02:45 +0800

    first version

---
+++ teachmall-auth/src/main/java/com/teachmall/ucenter/mapper/XcCompanyUserMapper.java
@@ -0,0 +1,15 @@
+package com.teachmall.ucenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.teachmall.ucenter.model.po.XcCompanyUser;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcCompanyUserMapper extends BaseMapper<XcCompanyUser> {
+
+}
---
+++ teachmall-auth/src/main/java/com/teachmall/ucenter/model/dto/XcUserExt.java
@@ -0,0 +1,13 @@
+package com.teachmall.ucenter.model.dto;
+
+import com.teachmall.ucenter.model.po.XcUser;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class XcUserExt extends XcUser {
+    //用户权限
+    List<String> permissions = new ArrayList<>();
+}
