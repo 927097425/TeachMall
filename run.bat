21da78aeb9405de0cf9fadaf458ff0957a880438
xiaochen <927097425@qq.com>
2024-08-19 17:31:39 +0800

    first version

---
+++ run.bat
@@ -0,0 +1,10 @@
+start /b java -Dfile.encoding=utf-8 -jar .\jar\teachmall-auth.jar
+start /b java -Dfile.encoding=utf-8 -jar .\jar\teachmall-checkcode.jar 
+start /b java -Dfile.encoding=utf-8 -jar .\jar\teachmall-gateway.jar 
+start /b java -Dfile.encoding=utf-8 -jar .\jar\teachmall-search.jar 
+start /b java -Dfile.encoding=utf-8 -jar .\jar\teachmall-content.jar 
+start /b java -Dfile.encoding=utf-8 -jar .\jar\teachmall-learning.jar 
+start /b java -Dfile.encoding=utf-8 -jar .\jar\teachmall-media.jar 
+start /b java -Dfile.encoding=utf-8 -jar .\jar\teachmall-orders.jar
+start /b java -Dfile.encoding=utf-8 -jar .\jar\teachmall-system.jar 
+start /b java -Dfile.encoding=utf-8 -jar .\jar\xxl-job-admin-2.3.1.jar
